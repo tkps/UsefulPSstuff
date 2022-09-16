139712d9b575c509581b751d07e26a8acb3c9b46
Tobias <tobias.karlsson@minchh.se>
2022-09-16 11:28:45 +0200

    Added synopsis

--- DHCP Stuff/DHCPOption6scan.psm1
+++ DHCP Stuff/DHCPOption6scan.psm1
@@ -1,10 +1,28 @@
+<#
+.Synopsis
+   Searches all Active Directory authorized DHCP servers for Option 6 pointing towards one or more specified IP Adresses
+.DESCRIPTION
+   Searches all Active Directory authorized DHCP servers for Option 6 pointing towards one or more specified IP Adresses
+.EXAMPLE
+   Search-DHCPScopesOption6 -IPAddress 10.0.10.0
+.EXAMPLE
+   Search-DHCPScopesOption6 -IPAddress 10.0.10.0,192.168.0.1
+.EXAMPLE
+   Resolve-DNSName dns.server.name | Search-DHCPSCopesOption6
+.INPUTS
+   IP address of the DNS Server which you want to find DHCP Scopes with Option 6 pointing towards
+.OUTPUTS
+   Output the information from DHCP Scope options with added Properties Server and Scope in a PSCustomObject
+.NOTES
+   Written by tkps@github
+#>
 Function Search-DHCPScopesOption6
 {
     Param(
         #IP Address of the DNS Server to look for in Option 6
         [Parameter(Mandatory=$true,
-                    ValueFromPipeline=$true)]
-                    [ipaddress]
+                    ValueFromPipelineByPropertyName=$true)]
+                    [ipaddress[]]
                     $IPAddress
     )
     $DHCPServers = Get-DhcpServerInDC
